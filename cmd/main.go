0aab49049236947d31cb1211c3ea0f6a54170a05
arodriguezce <anderson.rodriguez@avista.co>
2021-12-11 21:55:56 -0500

    feat: first fiber server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"anderson.co/web_service/shared/server"
+	"anderson.co/web_service/shared/server/fiber"
+)
+
+type app struct {
+	webServer server.WebServer
+}
+
+func NewApp(webServer server.WebServer) *app {
+	return &app{ webServer }
+}
 
 func main() {
-
+	fiberWebServer := fiber.NewServer()
+	app := NewApp(fiberWebServer)
+	app.webServer.Run("3030")
 }
